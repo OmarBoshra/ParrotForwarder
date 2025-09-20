fe2ad2572f20671bcdf71514184be5d431341b98
Omar Boshra Abouelela Taha Ahmed <o.ahmed@ostfalia.de>
2025-09-20 22:45:11 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Parrot forwarder
+
+## Goal
+1. Sanitize share data prior to AI handling
+2. Convert and categorize from multiple sources
+3. Review 
+4. (Optional) custom tagging
+
+  
+## Tech Stack
+Please utilize them as much as possible:
+- MVVM + Repository
+- Kotlin Flow
+- Dagger Hilt (Dependency Injection)
+- Retrofit (Networking)
+- Navigation Component
+- ktlint (Code Linting)
+
+
+### Optional
+- Epoxy (RecyclerView)
---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.navigation.safeargs)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+android {
+    namespace = "com.example.parrot"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.parrot"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+dependencies {
+
+    // Baseline & material components
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.circleimageview)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.play.services.basement)
+
+    // Viewmodel and lifecycle
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.process)
+    implementation(libs.androidx.lifecycle.service)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.compiler)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.okhttp)
+    implementation(libs.navigation.dynamic.features.fragment)
+
+
+    // Dependency injection
+    // Hilt
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // image loading
+    implementation(libs.coil)
+
+    // Epoxy
+    implementation(libs.epoxy)
+    kapt(libs.epoxy.processor)
+    implementation(libs.epoxy.paging3)
+
+    // Serialization and Parallelization
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+
+    // Data and view binding
+    implementation(libs.androidx.viewbinding)
+
+    // Tests
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
